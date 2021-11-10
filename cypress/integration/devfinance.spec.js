54992cdc1badfd144eef62887df74bf8bb8754be
Jucilene Barros <61738084+jucilene-barros-silva@users.noreply.github.com>
2021-11-10 03:36:54 -0300

    Update devfinance.spec.js

--- cypress/integration/devfinance.spec.js
+++ cypress/integration/devfinance.spec.js
@@ -1,8 +1,16 @@
 /// <reference types="cypress" />
 
+const { remove } = require("lodash");
+
 describe('DevFinance', () => {
+
+    // antes de cada Teste - beforeEach
+
+    beforeEach(() => {
+        cy.visit('https://devfinance-agilizei.netlify.app/#')
+    });
+
     it('Adicionar uma nova transação de entrada', () => {
-        cy.visit('https://devfinance-agilizei.netlify.app/#')
 
         //get & contains
         cy.get('a[onclick*=open]').click()
@@ -15,11 +23,8 @@ describe('DevFinance', () => {
         cy.get('table tbody tr').should('have.length', 1)
 
     });
-});
 
-describe('DevFinance', () => {
     it('Excluir uma transação de entrada', () => {
-        cy.visit('https://devfinance-agilizei.netlify.app/#')
 
         cy.get('a[onclick*=open]').click()
         cy.get('#description').type('Freela')
@@ -27,19 +32,14 @@ describe('DevFinance', () => {
         cy.get ('#date').type('2021-11-03')
 
         cy.contains('button', 'Salvar').click()
-
-        cy.get('table tbody tr td:last img').should('have.length', 1).click('center')
+        cy.get('img[onclick*=remove]').click()
+        // cy.get('table tbody tr td:last img').should('have.length', 1).click('center')
         
-
+        cy.get('table tbody tr').should('have.length', 0)
     });
-});
 
-
-describe('DevFinance', () => {
     it('Adicionar uma nova transação de saída', () => {
-        cy.visit('https://devfinance-agilizei.netlify.app/#')
 
-        //get & contains
         cy.get('a[onclick*=open]').click()
         cy.get('#description').type('Café')
         cy.get('#amount').type('-5')
@@ -50,11 +50,8 @@ describe('DevFinance', () => {
         cy.get('table tbody tr').should('have.length', 1)
 
     });
-});
 
-describe('DevFinance', () => {
     it('Excluir uma transação de saída', () => {
-        cy.visit('https://devfinance-agilizei.netlify.app/#')
 
         cy.get('a[onclick*=open]').click()
         cy.get('#description').type('Café')
@@ -65,6 +62,49 @@ describe('DevFinance', () => {
 
         cy.get('table tbody tr td:last img').should('have.length', 1).click('center')
 
+        cy.get('table tbody tr').should('have.length', 0)
+
+    });
+
+    //Possibilidade apresentada na correção do projeto
+
+    it('Excluir uma transação da listagem', () => {
+        
+        cy.get('a[onclick*=open]').click()
+        cy.get('#description').type('Freela')
+        cy.get('#amount').type('12')
+        cy.get ('#date').type('2021-11-03')
+
+        cy.contains('button', 'Salvar').click()
+
+        cy.get('table tbody tr').should('have.length', 1)
+
+        cy.contains('td', 'Freela') //  a partir de um texto ()
+          .parent() //voltarpara o elemento pai
+          .find('img[onclick*=remove]') // depois buscar com (find) um elemento específico
+          .click()
+
+        cy.get('table tbody tr').should('have.length', 0)
+
+    });
+
+    it('Excluir uma transação da listagem 2', () => {
+        
+        cy.get('a[onclick*=open]').click()
+        cy.get('#description').type('Freela')
+        cy.get('#amount').type('12')
+        cy.get ('#date').type('2021-11-03')
+
+        cy.contains('button', 'Salvar').click()
+
+        cy.get('table tbody tr').should('have.length', 1)
+
+        cy.contains('td', 'Freela')
+          .siblings() // lista os elementos irmãos 
+          .children('img[onclick*=remove]')
+          .click() 
+
+        cy.get('table tbody tr').should('have.length', 0)
+
     });
 });
-
