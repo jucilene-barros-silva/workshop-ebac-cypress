1834f5622258ce8b021f99083ff5fd45966c3a3a
Jucilene Barros <61738084+jucilene-barros-silva@users.noreply.github.com>
2021-11-10 03:44:59 -0300

    Update devfinance.spec.js

--- cypress/integration/devfinance.spec.js
+++ cypress/integration/devfinance.spec.js
@@ -33,37 +33,8 @@ describe('DevFinance', () => {
 
         cy.contains('button', 'Salvar').click()
         cy.get('img[onclick*=remove]').click()
-        // cy.get('table tbody tr td:last img').should('have.length', 1).click('center')
         
         cy.get('table tbody tr').should('have.length', 0)
-    });
-
-    it('Adicionar uma nova transação de saída', () => {
-
-        cy.get('a[onclick*=open]').click()
-        cy.get('#description').type('Café')
-        cy.get('#amount').type('-5')
-        cy.get ('#date').type('2021-11-03')
-
-        cy.contains('button', 'Salvar').click()
-
-        cy.get('table tbody tr').should('have.length', 1)
-
-    });
-
-    it('Excluir uma transação de saída', () => {
-
-        cy.get('a[onclick*=open]').click()
-        cy.get('#description').type('Café')
-        cy.get('#amount').type('-5')
-        cy.get ('#date').type('2021-11-03')
-
-        cy.contains('button', 'Salvar').click()
-
-        cy.get('table tbody tr td:last img').should('have.length', 1).click('center')
-
-        cy.get('table tbody tr').should('have.length', 0)
-
     });
 
     //Possibilidade apresentada na correção do projeto
